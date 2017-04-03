a07f8760b80687f14e2603367c1b49fd2e76a808
marcocelani <marco.celani@hotmail.it>
2017-04-03 16:08:22 +0200

    Unused method removed.

--- SporpingBike/io_util.js
+++ SporpingBike/io_util.js
@@ -5,7 +5,7 @@ var	readChunk = require('read-chunk');
 var imageType = require('image-type');
 
 var processFile = function(file, id, next){
-	var buffer = readChunk(file.path, 0, file.size, function(error, buffer){
+	readChunk(file.path, 0, file.size, function(error, buffer){
 		if(error){
 			next(new Error('Cannot read file:' + error.message), null);
 		} else {
@@ -21,31 +21,4 @@ var processFile = function(file, id, next){
 	});
 };
 
-/* NOT USED */
-var removeBike = function(doc, next){
-	if(doc){
-		fs.unlink('bike/' + doc.fileName, function(err){
-			if(err){
-				console.log('cannot unlink:' + err.message);
-				next(new Error(err.message));
-			}
-			next(null);
-		});
-	}
-};
-
-/* next(null, bike) is a random image file */
-/*var randomBike = function(next){
-	fs.readdir('./bike', function(err, fList){
-		if(err){
-			console.log(err.stack);
-			next(new Error('error getting randomBike:' + err.message));
-		} else {
-			next(null, fList[Math.floor(Math.random() * fList.length)]);	
-		}	
-	});
-};*/
-
 exports.processFile = processFile;
-//exports.removeBike = removeBike;
-//exports.randomBike = randomBike;
